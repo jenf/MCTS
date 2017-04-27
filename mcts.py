466ddc68f266052d86e7237533bce4ded7efd94e
Jen Freeman <jenf@users.noreply.github.com>
2017-04-27 17:38:17 +0100

    Begin refactor of mcts

--- mcts.py
+++ mcts.py
@@ -26,130 +26,142 @@ logger = logging.getLogger('MyLogger')
 
 
 class State():
-	NUM_TURNS = 10	
-	GOAL = 0
-	MOVES=[2,-2,3,-3]
-	MAX_VALUE= (5.0*(NUM_TURNS-1)*NUM_TURNS)/2
-	num_moves=len(MOVES)
-	def __init__(self, value=0, moves=[], turn=NUM_TURNS):
-		self.value=value
-		self.turn=turn
-		self.moves=moves
-	def next_state(self):
-		nextmove=random.choice([x*self.turn for x  in self.MOVES])
-		next=State(self.value+nextmove, self.moves+[nextmove],self.turn-1)
-		return next
-	def terminal(self):
-		if self.turn == 0:
-			return True
-		return False
-	def reward(self):
-		r = 1.0-(abs(self.value-self.GOAL)/self.MAX_VALUE)
-		return r
-	def __hash__(self):
-		return int(hashlib.md5(str(self.moves)).hexdigest(),16)
-	def __eq__(self,other):
-		if hash(self)==hash(other):
-			return True
-		return False
-	def __repr__(self):
-		s="Value: %d; Moves: %s"%(self.value,self.moves)
-		return s
-	
+    NUM_TURNS = 10  
+    GOAL = 0
+    MOVES=[2,-2,3,-3]
+    MAX_VALUE= (5.0*(NUM_TURNS-1)*NUM_TURNS)/2
+    num_moves=len(MOVES)
+    def __init__(self, value=0, moves=[], turn=NUM_TURNS):
+        self.value=value
+        self.turn=turn
+        self.moves=moves
+    def next_state(self):
+        nextmove=random.choice([x*self.turn for x  in self.MOVES])
+        next=State(self.value+nextmove, self.moves+[nextmove],self.turn-1)
+        return next
+    def terminal(self):
+        if self.turn == 0:
+            return True
+        return False
+    def reward(self):
+        r = 1.0-(abs(self.value-self.GOAL)/self.MAX_VALUE)
+        return r
+    def __hash__(self):
+        return int(hashlib.md5(str(self.moves)).hexdigest(),16)
+    def __eq__(self,other):
+        if hash(self)==hash(other):
+            return True
+        return False
+    def __repr__(self):
+        s="Value: %d; Moves: %s"%(self.value,self.moves)
+        return s
+    
 
 class Node():
-	def __init__(self, state, parent=None):
-		self.visits=1
-		self.reward=0.0	
-		self.state=state
-		self.children=[]
-		self.parent=parent	
-	def add_child(self,child_state):
-		child=Node(child_state,self)
-		self.children.append(child)
-	def update(self,reward):
-		self.reward+=reward
-		self.visits+=1
-	def fully_expanded(self):
-		if len(self.children)==self.state.num_moves:
-			return True
-		return False
-	def __repr__(self):
-		s="Node; children: %d; visits: %d; reward: %f"%(len(self.children),self.visits,self.reward)
-		return s
-		
+    def __init__(self, state, parent=None):
+        self.visits=1
+        self.reward=0.0 
+        self.state=state
+        self.children=[]
+        self.parent=parent  
+    def add_child(self,child_state):
+        child=Node(child_state,self)
+        self.children.append(child)
+    def update(self,reward):
+        self.reward+=reward
+        self.visits+=1
+    def fully_expanded(self):
+        if len(self.children)==self.state.num_moves:
+            return True
+        return False
+    def __repr__(self):
+        s="Node; children: %d; visits: %d; reward: %f"%(len(self.children),self.visits,self.reward)
+        return s
+        
 
 
-def UCTSEARCH(budget,root):
-	for iter in range(budget):
-		if iter%10000==9999:
-			logger.info("simulation: %d"%iter)
-			logger.info(root)
-		front=TREEPOLICY(root)
-		reward=DEFAULTPOLICY(front.state)
-		BACKUP(front,reward)
-	return BESTCHILD(root,0)
+class MonteCarloSearch(object):
+    def __init__(self, banditStrategy, debugIter=100):
+        self.banditStrategy=banditStrategy
+        self.debugIterations=debugIter
+      
+    def search(self,budget,root):
+        """Find best move for the current move"""
+        for iter in range(budget):
+            if iter%self.debugIterations==(self.debugIterations-1):
+                logger.info("simulation: %d"%iter)
+                logger.info(root)
+            front=self.TREEPOLICY(root)
+            reward=self.calculateReward(front.state)
+            self.updateNodes(front,reward)
+        return self.banditStrategy(root,0)
 
-def TREEPOLICY(node):
-	while node.state.terminal()==False:
-		if node.fully_expanded()==False:	
-			return EXPAND(node)
-		else:
-			node=BESTCHILD(node,SCALAR)
-	return node
+    def TREEPOLICY(self,node):
+        """Iterate down the tree until the the leaf (fully expanded)"""
+        while node.state.terminal()==False:
+            if node.fully_expanded()==False:    
+                return self.EXPAND(node)
+            else:
+                node=self.banditStrategy(node,SCALAR)
+        return node
 
-def EXPAND(node):
-	tried_children=[c.state for c in node.children]
-	new_state=node.state.next_state()
-	while new_state in tried_children:
-		new_state=node.state.next_state()
-	node.add_child(new_state)
-	return node.children[-1]
+    def EXPAND(self, node):
+        tried_children=[c.state for c in node.children]
+        new_state=node.state.next_state()
+        while new_state in tried_children:
+            new_state=node.state.next_state()
+        node.add_child(new_state)
+        return node.children[-1]
+
+    def calculateReward(self, state):
+        """Calculate the reward for the given state by finding the leaf"""
+        while state.terminal()==False:
+            state=state.next_state()
+        return state.reward()
+
+    def updateNodes(self, node,reward):
+        """Update nodes from the leaf to the root"""
+        while node!=None:
+            node.visits+=1
+            node.reward+=reward
+            node=node.parent
+        return
+
 
 #current this uses the most vanilla MCTS formula it is worth experimenting with THRESHOLD ASCENT (TAGS)
 def BESTCHILD(node,scalar):
-	bestscore=0.0
-	bestchildren=[]
-	for c in node.children:
-		exploit=c.reward/c.visits
-		explore=math.sqrt(math.log(2*node.visits)/float(c.visits))	
-		score=exploit+scalar*explore
-		if score==bestscore:
-			bestchildren.append(c)
-		if score>bestscore:
-			bestchildren=[c]
-			bestscore=score
-	if len(bestchildren)==0:
-		logger.warn("OOPS: no best child found, probably fatal")
-	return random.choice(bestchildren)
-
-def DEFAULTPOLICY(state):
-	while state.terminal()==False:
-		state=state.next_state()
-	return state.reward()
-
-def BACKUP(node,reward):
-	while node!=None:
-		node.visits+=1
-		node.reward+=reward
-		node=node.parent
-	return
+    bestscore=0.0
+    bestchildren=[]
+    for c in node.children:
+        exploit=c.reward/c.visits
+        explore=math.sqrt(math.log(2*node.visits)/float(c.visits))  
+        score=exploit+scalar*explore
+        if score==bestscore:
+            bestchildren.append(c)
+        if score>bestscore:
+            bestchildren=[c]
+            bestscore=score
+    if len(bestchildren)==0:
+        logger.warn("OOPS: no best child found, probably fatal")
+    return random.choice(bestchildren)
 
 if __name__=="__main__":
-	parser = argparse.ArgumentParser(description='MCTS research code')
-	parser.add_argument('--num_sims', action="store", required=True, type=int)
-	parser.add_argument('--levels', action="store", required=True, type=int, choices=range(State.NUM_TURNS))
-	args=parser.parse_args()
-	
-	current_node=Node(State())
-	for l in range(args.levels):
-		current_node=UCTSEARCH(args.num_sims/(l+1),current_node)
-		print("level %d"%l)
-		print("Num Children: %d"%len(current_node.children))
-		for i,c in enumerate(current_node.children):
-			print(i,c)
-		print("Best Child: %s"%current_node.state)
-		
-		print("--------------------------------")	
-			
-	
+    random.seed(0)
+    parser = argparse.ArgumentParser(description='MCTS research code')
+    parser.add_argument('--num_sims', action="store", required=True, type=int)
+    parser.add_argument('--levels', action="store", required=True, type=int, choices=range(State.NUM_TURNS))
+    args=parser.parse_args()
+    monte=MonteCarloSearch(BESTCHILD)
+    
+    current_node=Node(State())
+    for l in range(args.levels):
+        current_node=monte.search(args.num_sims/(l+1),current_node)
+        print("level %d"%l)
+        print("Num Children: %d"%len(current_node.children))
+        for i,c in enumerate(current_node.children):
+            print(i,c)
+        print("Best Child: %s"%current_node.state)
+        
+        print("--------------------------------")   
+            
+    
