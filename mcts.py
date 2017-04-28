a34aaefddaff7818baa5d7fbcf47c9b016c6f429
Jen Freeman <jenf@users.noreply.github.com>
2017-04-28 18:48:12 +0100

    Refactor mcts

--- mcts.py
+++ mcts.py
@@ -8,24 +8,24 @@ import argparse
 
 """
 A quick Monte Carlo Tree Search implementation.  For more details on MCTS see See http://pubs.doc.ic.ac.uk/survey-mcts-methods/survey-mcts-methods.pdf
-
-The State is just a game where you have NUM_TURNS and at turn i you can make
-a choice from [-2,2,3,-3]*i and this to to an accumulated value.  The goal is for the accumulated value to be as close to 0 as possible.
-
-The game is not very interesting but it allows one to study MCTS which is.  Some features 
-of the example by design are that moves do not commute and early mistakes are more costly.  
-
-In particular there are two models of best child that one can use 
 """
 
-#MCTS scalar.  Larger scalar will increase exploitation, smaller will increase exploration. 
-SCALAR=1/math.sqrt(2.0)
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger('MyLogger')
 
 
 class State():
+    """
+    The State is just a game where you have NUM_TURNS and at turn i you can make
+    a choice from [-2,2,3,-3]*i and this to to an accumulated value.  The goal is for the accumulated value to be as close to 0 as possible.
+
+    The game is not very interesting but it allows one to study MCTS which is.  Some features 
+    of the example by design are that moves do not commute and early mistakes are more costly.  
+
+    In particular there are two models of best child that one can use 
+    """
+
     NUM_TURNS = 10  
     GOAL = 0
     MOVES=[2,-2,3,-3]
@@ -35,23 +35,31 @@ class State():
         self.value=value
         self.turn=turn
         self.moves=moves
-    def next_state(self):
-        nextmove=random.choice([x*self.turn for x  in self.MOVES])
-        next=State(self.value+nextmove, self.moves+[nextmove],self.turn-1)
-        return next
+
+    def getMoves(self):
+        states=[]
+        for x in self.MOVES:
+            move=x*self.turn
+            states.append(State(self.value+move, self.moves+[move], self.turn-1))
+        return states
+
     def terminal(self):
         if self.turn == 0:
             return True
         return False
+
     def reward(self):
         r = 1.0-(abs(self.value-self.GOAL)/self.MAX_VALUE)
         return r
+
     def __hash__(self):
         return int(hashlib.md5(str(self.moves)).hexdigest(),16)
+
     def __eq__(self,other):
         if hash(self)==hash(other):
             return True
         return False
+
     def __repr__(self):
         s="Value: %d; Moves: %s"%(self.value,self.moves)
         return s
@@ -62,20 +70,32 @@ class Node():
         self.visits=1
         self.reward=0.0 
         self.state=state
-        self.children=[]
-        self.parent=parent  
-    def add_child(self,child_state):
-        child=Node(child_state,self)
-        self.children.append(child)
+        self.noChildren=-1
+        self.untried=[]
+        self.tried=[]
+        self.parent=parent
+
+    def getUnvisitedChild(self):
+        if self.noChildren==-1:
+            self.untried=self.state.getMoves()
+            self.noChildren=len(self.untried)
+        choice=random.choice(self.untried)
+        child=Node(choice, self)
+        self.tried.append(child)
+        self.untried.remove(choice)
+        return child
+
     def update(self,reward):
         self.reward+=reward
         self.visits+=1
-    def fully_expanded(self):
-        if len(self.children)==self.state.num_moves:
+
+    def fullyVisited(self):
+        if len(self.tried)==self.noChildren:
             return True
         return False
+
     def __repr__(self):
-        s="Node; children: %d; visits: %d; reward: %f"%(len(self.children),self.visits,self.reward)
+        s="Node; children: %d; visits: %d; reward: %f"%(len(self.tried),self.visits,self.reward)
         return s
         
 
@@ -84,6 +104,8 @@ class MonteCarloSearch(object):
     def __init__(self, banditStrategy, debugIter=100):
         self.banditStrategy=banditStrategy
         self.debugIterations=debugIter
+        #MCTS scalar.  Larger scalar will increase exploitation, smaller will increase exploration. 
+        self.scalar=1/math.sqrt(2.0)
       
     def search(self,budget,root):
         """Find best move for the current move"""
@@ -91,33 +113,18 @@ class MonteCarloSearch(object):
             if iter%self.debugIterations==(self.debugIterations-1):
                 logger.info("simulation: %d"%iter)
                 logger.info(root)
-            front=self.TREEPOLICY(root)
-            reward=self.calculateReward(front.state)
-            self.updateNodes(front,reward)
+            front=self.expandNodeUntilLeaf(root)
+            self.updateNodes(front,front.state.reward())
         return self.banditStrategy(root,0)
 
-    def TREEPOLICY(self,node):
+    def expandNodeUntilLeaf(self,node):
         """Iterate down the tree until the the leaf (fully expanded)"""
         while node.state.terminal()==False:
-            if node.fully_expanded()==False:    
-                return self.EXPAND(node)
+            if node.fullyVisited()==False:    
+                node=node.getUnvisitedChild()
             else:
-                node=self.banditStrategy(node,SCALAR)
+                node=self.banditStrategy(node,self.scalar)
         return node
-
-    def EXPAND(self, node):
-        tried_children=[c.state for c in node.children]
-        new_state=node.state.next_state()
-        while new_state in tried_children:
-            new_state=node.state.next_state()
-        node.add_child(new_state)
-        return node.children[-1]
-
-    def calculateReward(self, state):
-        """Calculate the reward for the given state by finding the leaf"""
-        while state.terminal()==False:
-            state=state.next_state()
-        return state.reward()
 
     def updateNodes(self, node,reward):
         """Update nodes from the leaf to the root"""
@@ -132,7 +139,7 @@ class MonteCarloSearch(object):
 def BESTCHILD(node,scalar):
     bestscore=0.0
     bestchildren=[]
-    for c in node.children:
+    for c in node.tried:
         exploit=c.reward/c.visits
         explore=math.sqrt(math.log(2*node.visits)/float(c.visits))  
         score=exploit+scalar*explore
@@ -157,8 +164,8 @@ if __name__=="__main__":
     for l in range(args.levels):
         current_node=monte.search(args.num_sims/(l+1),current_node)
         print("level %d"%l)
-        print("Num Children: %d"%len(current_node.children))
-        for i,c in enumerate(current_node.children):
+        print("Num Children: %d"%len(current_node.tried))
+        for i,c in enumerate(current_node.tried):
             print(i,c)
         print("Best Child: %s"%current_node.state)
         
